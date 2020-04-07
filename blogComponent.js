c6d72a77c4ddbf8afc9fe76bc7fef427aac7158f
Deepak Kendole <deepakdpk101@gmail.com>
2020-04-07 16:12:21 +0530

    update

--- blogComponent.js
+++ blogComponent.js
@@ -1,8 +1,8 @@
-const LikeButton = function() {
+const App = function() {
   return (
    <div>i am a like button</div>
   )
 }
 const domContainer = document.querySelector('#root'); //element is created in blogspot
 console.log(domContainer);
-ReactDOM.render(<LikeButton />, domContainer);
+ReactDOM.render(<App />, domContainer);
